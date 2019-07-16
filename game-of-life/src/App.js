73047cbed9dabf66e3812922ed658251e1c786cc
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-07-16 19:22:17 -0400

    Start and Stop seem to be working as intended now

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,31 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.scss';
 import './SCSS/index.module.scss';
-import Board from './components/Board/Board.js'
+import Board from './components/Board/Board.js';
 
 function App() {
   // STATE**********
   const [board, setBoard] = useState([]);
-  const [changeState, setChangeState] = useState(false)
+  const [changeState, setChangeState] = useState(false);
+
+
+  let timerId;
+  const startGame = () => {
+    timerId = window.setInterval(gameOfLife, 500)
+  }
+
+  const stopGame = (e) => {
+    e.preventDefault()
+    window.clearInterval(timerId)
+  }
+
+  const gameOfLife = () => {
+    console.log("loop")
+    for (let row = 0; row < board.length;  row++) {
+      for (let column = 0; column < board[row].length; column++) {
+        let count = 0;
+        console.log("Row: ", row, "Column: ", column)
+      }
+    }
+  }
 
   // MOUNT / EFFECT *************
   useEffect(() => {
-    let testArr = []
+    let testArr = [];
     // rows
     for (let i = 0; i < 16; i++) {
       // hold open the space to insert the colums
-      testArr[i] = []
+      testArr[i] = [];
       // columns
       for (let j = 0; j < 16; j++) {
         testArr[i][j] = 0;
       }
     }
-    console.log(testArr)
-    setBoard(testArr)
-  }, [])
+    console.log(testArr);
+    setBoard(testArr);
+  }, []);
+
   return (
     <div className="App">
-    <Board board={board} setBoard={setBoard} changeState={changeState} setChangeState={setChangeState}/>
+    <div className="boardContainer">
+      <Board
+        board={board}
+        setBoard={setBoard}
+        changeState={changeState}
+        setChangeState={setChangeState}
+      />
+    </div>
+    <div className="btnContainer">
+    <button onClick={() => startGame()}>Go</button>
+    <button onClick={(e) => stopGame(e)}>Halt</button>
+    </div>
     </div>
   );
 }
--- game-of-life/src/components/Board/Board.js
+++ game-of-life/src/components/Board/Board.js
@@ -2,42 +2,51 @@ import React, {useState, useEffect} from 'react';
 
 import styles from './Board.module.scss';
 
-const Board = ({ board, setBoard, changeState, setChangeState }) => {
-
+const Board = ({board, setBoard, changeState, setChangeState}) => {
   //useEffect(() => {
   //}, [newBoard])
-  const alterCell = (e) => {
+  const alterCell = e => {
     e.preventDefault();
-    const row =  e.target.dataset.row
-    const column = e.target.dataset.col
+    const row = e.target.dataset.row;
+    const column = e.target.dataset.col;
     let newBoard = board;
-    newBoard[row][column] == 0 ? newBoard[row][column] = 1 : newBoard[row][column] = 0
-    console.log("CELL :", newBoard[row][column])
-    console.log("newBoard: ", newBoard)
-    setBoard(newBoard)
+    newBoard[row][column] === 0
+      ? (newBoard[row][column] = 1)
+      : (newBoard[row][column] = 0);
+    //console.log("CELL :", newBoard[row][column])
+    //console.log("newBoard: ", newBoard)
+    setBoard(newBoard);
     if (changeState === false) {
-      setChangeState(true)
+      setChangeState(true);
     } else {
-      setChangeState(false)
+      setChangeState(false);
     }
-  }
+  };
 
   return (
     <section className={styles.gridContainer}>
-    {
-      board.map(( row, rowIndex ) => {
+      {board.map((row, rowIndex) => {
         return (
           <div key={`${rowIndex}`} className={styles.row}>
-          {row.map(( column, colIndex ) => {
-            return (
-              <div key={`${colIndex} ${rowIndex}}`} className={column === 1 ? `${styles.column} ${styles.alive}` : `${styles.column}`} data-col={`${colIndex}`} data-row={`${rowIndex}`} onClick={(e) => alterCell(e)} />
-            )
-          })}
+            {row.map((column, colIndex) => {
+              return (
+                <div
+                  key={`${colIndex} ${rowIndex}}`}
+                  className={
+                    column === 1
+                      ? `${styles.column} ${styles.alive}`
+                      : `${styles.column}`
+                  }
+                  data-col={`${colIndex}`}
+                  data-row={`${rowIndex}`}
+                  onClick={e => alterCell(e)}
+                />
+              );
+            })}
           </div>
-        )
-      })
-    }
-    {/*<div className={styles.row}>
+        );
+      })}
+      {/*<div className={styles.row}>
         <div className={styles.column} />
         <div className={styles.column} />
         <div className={styles.column} />
